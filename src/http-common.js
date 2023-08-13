ab9373000a25db6443de58aa8fbcf793d81f304b
bensy111 <96622952+bensy111@users.noreply.github.com>
2023-08-13 19:21:03 +0300

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 export default axios.create({
   // connecting to our local db
-  baseURL: "http://localhost:8086",
-  //baseURL: "http://52.90.135.103:8086/students",
+ // baseURL: "http://localhost:8086",
+  baseURL: "http://52.90.135.103:8086/students",
   headers: {
     "Content-type": "application/json",
   },
